bdd968c63b8b0b2df6707d592cf1f05f11051545
botiak-v <125253441+botiak-v@users.noreply.github.com>
2023-05-19 17:56:13 +0300

    Update face.js

--- face.js
+++ face.js
@@ -21,7 +21,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 	loader.load(
 		'batman.glb',
 		function ( gltf ) {
-			gltf.scene.scale.set(0.4, 0.4, 0.4);
+			gltf.scene.scale.set(0.5, 0.5, 0.5);
 			gltf.scene.position.y=-0.1;
 			gltf.scene.position.z=-0.2;
 
